8b5259c4c9af2b9989d2a9c6ca2b342eeab7e932
mapena <mapena@gmail.com>
2020-08-15 16:55:17 -0300

    .

--- descargas/py1/app-html.py
+++
@@ -1,9 +0,0 @@
-# escribe-html.py
-import urllib.request, urllib.error, urllib.parse, webbrowser
-f = open('holamundo.html','w')
-mensaje = """<html>
-<head></head>
-<body><p>Hola Mundo!</p></body>
-</html>"""
-f.write(mensaje)
-f.close()

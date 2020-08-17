94e5fd560c2e3daece447d05ff1f3fb927335a42
mapena <mapena@gmail.com>
2020-08-17 11:50:12 -0300

    .

--- flask-crud-contacts-app-master/App.py
+++ flask-crud-contacts-app-master/App.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def Index():
      return 'Hola Marce2'
 
+@app.route('/add_contact')
+def add_contact():
+         return 'Add contacto'
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)

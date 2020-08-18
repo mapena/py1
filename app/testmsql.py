48551e0fe296e03f9c38a9632617ecd71a7b88e4
mapena <mapena@gmail.com>
2020-08-18 11:48:35 -0300

    .

--- app/testmsql.py
+++ app/testmsql.py
@@ -1,30 +1,28 @@
 import mysql.connector
 
-
-
-try:
-  mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="mydbx"
-  )
-
-except:
-  if (mydb):
-    # Carry out normal procedure
-    print ("Connection successful")
-  else:
-    # Terminate
-    print ("Connection unsuccessful")
+def myconectar():
+  try:
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="",
+      database="mydb"
+   )
+  except:
+    print("Error de conexcion")
     exit()
+#  if (mydb):
+#    # Carry out normal procedure
+#    print ("Connection successful")
+#  else:
+#    # Terminate
+#    print ("Connection unsuccessful")
+#    exit()
 
+myconectar()
 mycursor = mydb.cursor()
-
 mycursor.execute("SELECT * FROM registros")
 print("********************************")
-
 myresult = mycursor.fetchall()
-
 for x in myresult:
   print(x)

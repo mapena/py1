5b3c5a20f45f3b1f3d3a2d81afd1046dc6cf4182
mapena <mapena@gmail.com>
2020-08-18 16:08:12 -0300

    .

--- flask-crud-contacts-app-master/App.py
+++ flask-crud-contacts-app-master/App.py
@@ -12,7 +12,7 @@ mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    database="mydbx"
+    database="mydb"
 )
 mycursor = mydb.cursor()          
 #--------------------------------------------------------------------------------------------------------

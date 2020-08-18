7dc6951b6b22b5a3653ede5eee3fdea335a6d987
mapena <mapena@gmail.com>
2020-08-17 23:16:04 -0300

    .

--- flask-crud-contacts-app-master/App.py
+++ flask-crud-contacts-app-master/App.py
@@ -38,16 +38,22 @@ def add_reg():
         flash(str(mycursor.rowcount) + " Registro/s Agregado/s")
         return redirect(url_for('Index'))  # se hace refencia a la fun Index que apunta a index.html
 
-@app.route('/delete_reg/<string:id>')
+@app.route('/delete_reg/<string:id>')    #no hace falta indicar q es un string, se puede colocar id directamente.
 def delete_reg(id):
     sql = "DELETE FROM registros WHERE clave like '" + id + "%'"
     #print("sql",sql)
-    #mycursor.execute('DELETE FROM registros WHERE clave like {0}',id)
+    #mycursor.execute('DELETE FROM registros WHERE clave like {0}',format(id))
     mycursor.execute(sql)
     mydb.commit()
     flash(str(mycursor.rowcount) + " Registro/s Eliminado/s")
     return redirect(url_for('Index'))  # se hace refencia a la fun Index que apunta a index.html
 
+@app.route('/edit_reg/<id>')   #id recibe un parametro
+def get_reg(id):
+    mycursor.execute("SELECT * FROM registros WHERE clave = %s",(id))
+    myresult = mycursor.fetchall()
+    print(myresult)
+    return "fun edit_reg" 
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
